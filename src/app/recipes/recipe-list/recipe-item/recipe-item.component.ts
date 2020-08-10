06a968749efbb1195fd91604af5d74be2e7b217c
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-10 16:02:48 -0300

    Pass recipe data with property binding.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
 
@@ -9,10 +9,7 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 
-  // recipes: Recipe[] = [
-  //   new Recipe('A Test', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-  //   new Recipe('A Test', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-  // ]
+@Input() recipe: Recipe
 
   constructor() { }
 
