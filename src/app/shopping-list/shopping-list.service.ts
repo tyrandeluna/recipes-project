cdb041f0cbe014e8cc0d4ecb3236586cc513f5d8
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-13 18:57:23 -0300

    allow the selection of items in the list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,11 @@
-import { EventEmitter } from '@angular/core';
 import { Subject } from 'rxjs'
 
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>()
-
+    startedEditing = new Subject<number>()
+    
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
