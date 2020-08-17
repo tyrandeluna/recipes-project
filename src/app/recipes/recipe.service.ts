ecb6fa68c70a096297776f3dddbcd69737a52c03
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-17 13:38:23 -0300

    submit the recipe edit form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import { Recipe } from './recipe.model'
 import { Injectable } from '@angular/core'
 import { Ingredient } from '../shared/ingredient.model'
 import { ShoppingListService } from '../shopping-list/shopping-list.service'
+import { Subject } from 'rxjs'
 
 @Injectable()
 export class RecipeService {
+
+    recipesChanged = new Subject<Recipe[]>()
     
     private recipes: Recipe[] = [
         new Recipe(
@@ -35,7 +38,18 @@ export class RecipeService {
     getRecipe(id: number) {
         return this.recipes.slice()[id]
     }
+
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients)
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe)
+        this.recipesChanged.next(this.recipes.slice())
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice())
     }
 }
