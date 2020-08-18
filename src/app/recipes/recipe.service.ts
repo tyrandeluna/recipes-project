02a249b0a9529d8591a420a377d51b541304168d
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-18 15:21:21 -0300

    transform response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,27 @@ export class RecipeService {
 
     recipesChanged = new Subject<Recipe[]>()
     
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Egg Noodles With Prawns', 
-            'A quick midweek meal!', 
-            'https://realfood.tesco.com/media/images/1400x919-EggNoodlesPrawn-18d15cbf-c98a-4ede-8d48-19b59ec6af33-0-1400x919.jpg',
-            [
-                new Ingredient('Red Onion', 1),
-                new Ingredient('Egg', 5)
-            ]),
-        new Recipe(
-            'Sunshine Pizza', 
-            'Transport yourself to the Mediterranean!', 
-            'https://realfood.tesco.com/media/images/RFO-1400x919-Sunshine-pizza-fc90bdc6-bc4f-46cf-b63a-1ff7bc928bc9-0-1400x919.jpg',
-            [
-                new Ingredient('Egg',2),
-                new Ingredient('Yellow Pepper', 1),
-                new Ingredient('Orange Pepper', 1)
-            ])
-    ]
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Egg Noodles With Prawns', 
+    //         'A quick midweek meal!', 
+    //         'https://realfood.tesco.com/media/images/1400x919-EggNoodlesPrawn-18d15cbf-c98a-4ede-8d48-19b59ec6af33-0-1400x919.jpg',
+    //         [
+    //             new Ingredient('Red Onion', 1),
+    //             new Ingredient('Egg', 5)
+    //         ]),
+    //     new Recipe(
+    //         'Sunshine Pizza', 
+    //         'Transport yourself to the Mediterranean!', 
+    //         'https://realfood.tesco.com/media/images/RFO-1400x919-Sunshine-pizza-fc90bdc6-bc4f-46cf-b63a-1ff7bc928bc9-0-1400x919.jpg',
+    //         [
+    //             new Ingredient('Egg',2),
+    //             new Ingredient('Yellow Pepper', 1),
+    //             new Ingredient('Orange Pepper', 1)
+    //         ])
+    // ]
+
+    private recipes: Recipe[] = []
     
     constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -20,6 +21,12 @@ export class DataStorageService {
     fetchRecipes() {
         this.http
         .get<Recipe[]>('https://ng-course-recipe-book-11ddb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                //verify if the recipe have any ingredients or not, if not, ingredients will be a empty array
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            })
+        }))
         .subscribe(recipes => {
             this.recipeService.setRecipes(recipes)
         })
