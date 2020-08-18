3e8f5c5f83be72fcbd9a04c067793f5c0aedc051
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-18 14:04:02 -0300

    store recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { DataStorageService } from '../shared/data-storage.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+  
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+  }
 }
