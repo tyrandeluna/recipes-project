0c8907e59a3cff20892c8ba22b1f51006a551f0f
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-19 14:22:55 -0300

    prepare the signup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+    kind: string
+    idToken: string
+    email: string
+    refreshToken: string
+    expiresIn: string
+    localId: string
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    signup(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCf1vl7PVc3UEbo_OxwjlDYJWi5t-6pbg8',
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        )
+    }
+}
